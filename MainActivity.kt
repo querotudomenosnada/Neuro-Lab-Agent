a7ba14de6b3b76af742b983872a346996310bc54
querotudomenosnada <querotudomenosnada@gmail.com>
2026-02-19 01:18:53 -0300

    Delete MainActivity.kt

--- MainActivity.kt
+++
@@ -1,4 +0,0 @@
-package com.neurolab.agent
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-class MainActivity:AppCompatActivity(){override fun onCreate(b:Bundle?){super.onCreate(b)}}
